9fe8206124ce80a81d5b422d9908adad569d582e
sheriby <hony_sher@foxmail.com>
2020-07-22 15:59:22 +0800

    init the blog project

---
+++ src/test/java/com/sher/simpleblog/SimpleBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sher.simpleblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
